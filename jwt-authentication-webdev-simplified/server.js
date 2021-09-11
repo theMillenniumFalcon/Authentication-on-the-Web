e7aced1678af71f90035be7d1115831c97b5b41d
= <=>
2021-09-11 16:34:35 +0530

    web-dev-simplified not completed

--- jwt-authentication-webdev-simplified/server.js
+++ jwt-authentication-webdev-simplified/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
@@ -20,6 +22,10 @@ app.get('/posts', (req, res) => {
 app.post("/login", (req, res) => {
     // Authenticate user
     const username = req.body.username
+
+    const user = { name: username }
+    jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({ accessToken: accessToken })
 
 })
 
