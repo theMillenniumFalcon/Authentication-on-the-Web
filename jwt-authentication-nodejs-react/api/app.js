3cc4c76c59999df9f8b8512ad279731bfbe023b2
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-09-28 18:00:26 +0530

    jwt-verification-complete

--- jwt-authentication-nodejs-react/api/app.js
+++ jwt-authentication-nodejs-react/api/app.js
@@ -39,6 +39,22 @@ app.post("/api/login", (req, res) => {
     }
 })
 
+const verify = (req, res, next) => {
+    const authHeader = req.headers.authorization
+    if(authHeader) {
+        const token = authHeader.split(" ")[1]
+        jwt.verify(token, "mySecretKey", (err, user) => {
+            if(err) { 
+                return res.status(403).json("Token is not valid")
+            }
+            req.user = user
+            next()
+        })
+    } else {
+        res.status(401).json("You are not authenticated!")
+    }
+}
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`)
 })
