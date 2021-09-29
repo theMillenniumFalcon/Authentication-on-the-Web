d5fbd036a9c801fc88c46c4d241fc51ef18167b5
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-09-29 19:53:01 +0530

    Login_complete

--- mern_stack_advance_authentication/controllers/auth.js
+++ mern_stack_advance_authentication/controllers/auth.js
@@ -2,6 +2,7 @@ const User = require('../models/User')
 
 const register = async(req, res, next) => {
     const {username, email, password}  = req.body
+
     try {
         const user = await User.create({
             username, email, password
@@ -10,16 +11,37 @@ const register = async(req, res, next) => {
             success: true,
             user
         })
-    } catch(err) {
+    } catch(error) {
         res.status(500).json({
             success: false,
-            err: err.message,
+            error: error.message,
         })
     }
 }
 
-const login = (req, res, next) => {
-    res.send("Login route")
+const login = async (req, res, next) => {
+    const { email, password } = req.body
+    if(!email || !password) { 
+        res.status(400).json({ success: false, error: "Please provide email and password." })
+    }
+
+    try {
+        const user = await User.findOne({ email }).select("+password")
+        if(!user) {
+            res.status(404).json({ success: false, error: "Invalid credentials"})
+        }
+        const isMatch = await user.matchPasswords(password)
+        if(!isMatch) {
+            res.status(404).json({success: false, error: "Invalid credentials"})
+        }
+        res.status(200).json({
+            success: true,
+            token: "ddfhuewr",
+        })
+    } catch(error) {
+        res.status(500).json({ success: false, error: error.message })
+    }
+
 }
 
 const forgotpassword = (req, res, next) => {
