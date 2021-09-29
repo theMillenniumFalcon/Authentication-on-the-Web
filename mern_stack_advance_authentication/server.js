7e252d93811839bbffd86b52c77ff52a721b1e8c
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-09-29 18:58:15 +0530

    register_route_working

--- mern_stack_advance_authentication/server.js
+++ mern_stack_advance_authentication/server.js
@@ -1,12 +1,26 @@
+require('dotenv').config({path: "./config.env"})
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000
-require('dotenv').config({path: "./config.env"})
+const connectDB = require('./db/connect')
 // this allows us to get data from the body
 app.use(express.json())
 
 app.use('/api/auth', require('./routes/auth'))
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}`)
+const server = async () => {
+    try {
+        await connectDB(process.env.MONGO_URL)
+        app.listen(port, () => {
+            console.log(`Connected to database and listening on port ${port}...`)
+        })
+    } catch (err) {
+        console.error(err)
+    }
+}
+server()
+
+process.on('uncaughtException', (err, promise) => {
+    console.log(`Logged Error: ${err}`)
+    server.close(() => process.exit(1))
 })
