c46de5cf727c6fe1f8b6c2ea51c541baa8268102
theMillenniumFalcon <nishankpr2002@gmail.com>
2021-09-29 17:21:02 +0530

    register_route_completed

--- mern_stack_advance_authentication/controllers/auth.js
+++ mern_stack_advance_authentication/controllers/auth.js
@@ -1,5 +1,21 @@
-const register = (req, res, next) => {
-    res.send("Register route")
+const User = require('../models/User')
+
+const register = async(req, res, next) => {
+    const {username, email, password}  = req.body
+    try {
+        const user = await User.create({
+            username, email, password
+        })
+        res.status(200).json({
+            success: true,
+            user
+        })
+    } catch(err) {
+        res.status(500).json({
+            success: false,
+            err: err.message,
+        })
+    }
 }
 
 const login = (req, res, next) => {
