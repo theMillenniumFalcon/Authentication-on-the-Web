272b1a71574c33757a82fafc286b504c23ca692d
Nishank <nishankpr2002@gmail.com>
2021-11-10 22:45:00 +0530

    navbar_added

---
+++ google_authentication/src/components/navbar/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from "styled-components"
+
+
+const Navbar = () => {
+    return (
+        <NavbarContainer>
+            Hemlo
+        </NavbarContainer>
+    )
+}
+
+const NavbarContainer = styled.div`
+
+`;
+
+export default Navbar
