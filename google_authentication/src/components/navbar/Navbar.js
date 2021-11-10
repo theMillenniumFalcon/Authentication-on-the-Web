c1e3e3e89dab34393e6a34720913a5ad633b772c
Nishank <nishankpr2002@gmail.com>
2021-11-11 00:02:32 +0530

    navbar_completed

--- google_authentication/src/components/navbar/Navbar.js
+++ google_authentication/src/components/navbar/Navbar.js
@@ -1,17 +1,57 @@
 import React from 'react'
 import styled from "styled-components"
 
+const NavbarContainer = styled.div`
+height: 50px;
+background-color: rgb(13, 17, 19);
+color: white;
+display: flex;
+align-items: center;
+justify-content: space-around;
+`;
+
+const Logo = styled.div`
+font-size: 20px;
+font-weight: bold;
+`;
+
+const List = styled.ul`
+display: flex;
+align-items: center;
+list-style: none;
+`;
+
+const ListItem = styled.li`
+margin-right: 20px;
+font-weight: 500;
+curosr: pointer;
+`;
+
+const Avatar = styled.div`
+img {
+    width: 32px;
+    height: 32px;
+    border-radius: 50%;
+    object-fit: cover;
+}
+`;
 
 const Navbar = () => {
     return (
         <NavbarContainer>
-            Hemlo
+            <Logo>React App</Logo>
+            <List>
+                <ListItem>
+                        <Avatar>
+                            <img src="https://media.istockphoto.com/vectors/portrait-of-a-shorthaired-girl-androgin-lookingwith-asidelongglance-vector-id1190626181?k=20&m=1190626181&s=612x612&w=0&h=cqzcsUXp3pPDeFYJqspZY7amsnGzv9X9ZGQatwUeKEo=" 
+                            alt="" />
+                        </Avatar>
+                </ListItem>
+                <ListItem>Nishank</ListItem>
+                <ListItem>Logout</ListItem>
+            </List>
         </NavbarContainer>
     )
 }
-
-const NavbarContainer = styled.div`
-
-`;
 
 export default Navbar
