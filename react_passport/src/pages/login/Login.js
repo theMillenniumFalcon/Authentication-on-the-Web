33fb6faafc2a00d1399cc4d174856fd2d81864e2
Nishank <nishankpr2002@gmail.com>
2021-11-12 16:27:16 +0530

    Login_added

---
+++ react_passport/src/pages/login/Login.js
@@ -0,0 +1,72 @@
+import React from 'react'
+import styled from "styled-components"
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import AppleIcon from '@material-ui/icons/Apple';
+import GitHubIcon from '@material-ui/icons/GitHub';
+
+const Login = () => {
+    return (
+        <LoginContainer>
+            <LoginTitle>Choose a Login Method</LoginTitle>
+            <LoginWrapper>
+                <LeftContainer>
+                    <LoginButton>
+                        <Icon>
+                            <FacebookIcon/>
+                        </Icon>
+                    </LoginButton>
+                    <LoginButton>
+                        <Icon>
+                            <AppleIcon/>
+                        </Icon>
+                    </LoginButton>
+                    <LoginButton>
+                        <Icon>
+                            <GitHubIcon/>
+                        </Icon>
+                    </LoginButton>
+                </LeftContainer>
+                <RightContainer>
+                    <input type='text' plceholder='Username' />
+                    <input type='text' plceholder='Password' />
+                    <Button>Submit</Button>
+                </RightContainer>
+            </LoginWrapper>
+        </LoginContainer>
+    )
+}
+
+const LoginContainer = styled.div`
+
+`;
+
+const LoginTitle = styled.h1`
+
+`;
+
+const LoginWrapper = styled.div`
+
+`;
+
+const LeftContainer = styled.div`
+
+`;
+
+const RightContainer = styled.div`
+
+`;
+
+const LoginButton = styled.div`
+
+`;
+
+const Icon = styled.div`
+
+`;
+
+const Button = styled.div`
+
+`;
+
+export default Login
---
+++ react_passport/src/pages/post/Post.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import styled from "styled-components"
+import { posts } from "../../data"
+
+const Post = () => {
+    return (
+        <PostContainer>
+            <PostImage>
+                <img src="" alt="" />
+            </PostImage>
+            <PostTitle>
+
+            </PostTitle>
+            <PostDesc>
+
+            </PostDesc>
+            <PostLongDesc>
+
+            </PostLongDesc>
+        </PostContainer>
+    )
+}
+
+const PostContainer = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+`;
+
+const PostImage = styled.div`
+img {
+    width: 100%;
+    height: 300px;
+    object-fit: cover;
+    margin-bottom: 20px;
+}
+`;
+
+const PostTitle = styled.h2`
+
+`;
+
+const PostDesc = styled.div`
+padding: 0 100px;
+foont-size: 24px;
+font-weight: 300;
+color: gray;
+margin-top: 50px;
+`;
+
+const PostLongDesc = styled.div`
+padding: 100px;
+padding-top: 50px;
+font-size: 18px;
+line-height: 24px;
+`;
+
+export default Post
